2f4affd630ef9c27a61f15817a7d397cff9dd9db
Wannes Salomé <mail@wannessalome.nl>
2020-07-18 15:21:12 +0200

    Fix sanity-webhook

--- web/src/functions/sanity-webhook.ts
+++ web/src/functions/sanity-webhook.ts
@@ -2,14 +2,16 @@ import 'cross-fetch/polyfill';
 import { Handler } from 'aws-lambda';
 import * as yup from 'yup';
 
+const { GATSBY_SANITY_DATASET, GATSBY_SANITY_PROJECT_ID, GITHUB_TOKEN } = process.env;
+
 const requestBodySchema = yup
   .object({
     transactionId: yup.string().required(),
     projectId: yup
       .string()
-      .matches(new RegExp(process.env.GATSBY_SANITY_PROJECT_ID!), 'Project ID not valid')
+      .matches(new RegExp(GATSBY_SANITY_PROJECT_ID!), 'Project ID not valid')
       .required(),
-    dataset: yup.string().matches(/production/, 'Dataset not valid'),
+    dataset: yup.string().matches(new RegExp(GATSBY_SANITY_DATASET!), 'Dataset not valid'),
     ids: yup.object(),
   })
   .required();
@@ -33,20 +35,20 @@ export const handler: Handler = async (event) => {
     };
   }
 
-  const response = await fetch(
-    'https://api.github.com/repos/waspeer/greet-mertens/actions/workflows/build.yml/dispatches',
+  await fetch(
+    'https://api.github.com/repos/waspeer/greet-mertens/actions/workflows/1922885/dispatches',
     {
       method: 'POST',
       headers: {
         Accept: 'application/vnd.github.v3+json',
         'Content-Type': 'application/json; charset=utf-8',
+        Authorization: `Basic ${Buffer.from(`waspeer:${GITHUB_TOKEN}`).toString('base64')}`,
       },
       body: JSON.stringify({ ref: 'main' }),
     },
   );
 
   return {
-    statusCode: 200,
-    body: JSON.stringify(response),
+    statusCode: 204,
   };
 };
