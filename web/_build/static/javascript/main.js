47b478b73ebc6558bcfd433f6aca924db9dd76a5
Wannes Salomé <mail@wannessalome.nl>
2021-07-09 21:16:34 +0200

    Revert "Upgrade deps"
    This reverts commit 532c764b60deb0d8b57f6ccb2be54c66f62218e6.

--- web/_build/static/javascript/main.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  // src/javascript/images.ts
-  document.querySelectorAll("img").forEach((img) => {
-    img.dataset.loaded = "false";
-    img.addEventListener("load", () => {
-      img.dataset.loaded = "true";
-    });
-  });
-})();
