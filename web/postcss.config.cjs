d8bb653658bdc3e566ef009c13858d1074de7bcc
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2022-12-13 15:12:53 +0100

    fix: native pnpm install + open-props

--- web/postcss.config.cjs
+++ web/postcss.config.cjs
@@ -1,10 +1,7 @@
-const OpenProps = require('open-props');
-const jitProps = require('postcss-jit-props');
 const presetEnv = require('postcss-preset-env');
 
 module.exports = {
   plugins: [
-    jitProps(OpenProps),
     presetEnv({
       features: {
         'nesting-rules': true,
